ac236a388406a2a9bfc872d44c9f7e955b1628dd
Nathan Hensher <nhensh@gmail.com>
2024-11-06 14:50:51 +0000

    fix: firebase deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASEPATH : "",
-    reactStrictMode: false,
-    swcMinify: true,
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.NEXT_PUBLIC_BASEPATH
+      : "",
+  reactStrictMode: false,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
